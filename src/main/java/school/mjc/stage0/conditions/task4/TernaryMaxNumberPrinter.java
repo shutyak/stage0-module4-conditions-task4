88dab6c881103523abe9acad8a4ff7db8d1806d4
vlshutyak <vl.shutyak@gmail.com>
2023-04-13 21:52:13 +0300

    s0-m4-t4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first>second?first:second;
+        max = max>third?max:third;
+        System.out.println(max);
     }
 }
